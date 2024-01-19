03abca82d898d3cd9be91c7285f2f9b4f422dd10
Youssef Arrassen <arrassen.youssef@gmail.com>
2024-01-19 14:54:44 +0100

    update

--- main.py
+++ main.py
@@ -179,11 +179,124 @@ def download_data():
 
     pass
 
+def collect_data_andcompare():
+    url = 'https://craftpix.net/all-game-assets/'
+    driver.get(url)
+
+    wait_for_element(driver, By.XPATH, '//*[@id="wrap"]/section/main')
+
+    page_num = 1
+    page_limit = 98
+    data = {}  # Initialize data here
+    while True:
+        try:
+            
+            next_page_url = f'{url}page/{page_num}/'
+            print("Navigated to the next page:", next_page_url)
+            driver.get(next_page_url)
+
+            wait_for_element(driver, By.XPATH, '//*[@id="wrap"]/section/main')
+
+            print("Navigated to the next page.")
+
+            main_section = driver.find_element(by=By.XPATH, value='//*[@id="wrap"]/section/main')
+            data[page_num] =  extract_data_from_page(main_section)
+
+            print("Data extracted from the current page.")
+        except NoSuchElementException:
+            print("No more pages.")
+            break
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            #print stack trace
+
+            traceback.print_exc()
+
+            print("An error occurred during navigation or data extraction.")
+            break
+        page_num += 1
+        if page_num > page_limit:
+            print("Page limit reached.")
+            break
+    
+
+
+    driver.quit()
+
+    # Save data to a json file with 4 spaces indentation
+    with open('tocompare.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+    #compare data.json and tocompare.json
+    #load json data
+    data = []
+    with open('data.json') as json_file:
+        tmp = json.load(json_file)
+        
+        for page in tmp:
+            for item in tmp[page]:
+                data.append(item)
+        
+
+        
+    to_compare = []
+    with open('tocompare.json') as json_file:
+        tmp = json.load(json_file)
+        for page in tmp:
+            for item in tmp[page]:
+                to_compare.append(item)
+
+    
+    missing = {}
+    #compare data and to_compare links
+    for item in to_compare:
+        found = False
+        for item2 in data:
+            if item['Link'] == item2['Link']:
+                found = True
+                break
+        if not found:
+            print("Not found:", item['Link'])
+            print("Title:", item['Title'])
+            print("Category:", item['Category'])
+            print("Price:", item['Price'])
+            print("Link:", item['Link'])
+            print("")
+            #create missing.json similar to data.json but have number 1 only with the missing links
+            missing["1"] = item
+           
+            
+
+                
+
+        
+        if found:
+            print("Found:", item['Link'])
+            print("Title:", item['Title'])
+            print("Category:", item['Category'])
+            print("Price:", item['Price'])
+            print("Link:", item['Link'])
+            print("")
+
+    with open('missing.json', 'w') as outfile:
+                json.dump(missing, outfile, indent=4)
+    
+    print("missing links added to missing.json")
+
+            
+
+                    
+    pass
+
 if __name__ == "__main__":
     #collect_data()
     
-    download_data()
-    # Exit after collecting data to prevent the following code from running
+    #download_data()
+
+    #collect_data and compare to data.json
+    collect_data_andcompare ()
+
+
     exit()
 
 
